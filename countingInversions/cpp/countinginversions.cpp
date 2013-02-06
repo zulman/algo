0182204887c301303a5a403712c3fc98246ab80e
zulman <zulman@bk.ru>
2013-02-06 12:32:32 +0400

    refactored, test data updated

--- countingInversions/cpp/countinginversions.cpp
+++ countingInversions/cpp/countinginversions.cpp
@@ -5,28 +5,10 @@
  * Created on February 4, 2013, 10:09 AM
  */
 
-#include <string>
+
 #include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cstdlib>
-#include <cstring>
-#include <cassert>
 #include <iostream>
-#include <sstream>
-#include <cstddef>
-#include <algorithm>
-#include <utility>
-#include <iterator>
-#include <numeric>
-#include <list>
-#include <complex>
-#include <cstdio>
-#include <climits>
 #include <fcntl.h>
-#include <unistd.h>
 
 using namespace std;
 
@@ -64,68 +46,89 @@ static void redirect(int argc, const char **argv) {
   }
 }
 
-int merge(int a[], int left, int mid, int right) {
-  int invCount = 0;
-
-  int i = 1;
-  int j = 1;
-  int n = right - left + 1;
-  int hsize = mid - left;
-  int d[n];
-
-  for (int k = 1; k < n; k++) {
-    int b = a[left + i];
-    int c = a[right + j];
-    if (b <= c) {
+unsigned long merge(int a[], int d[], int low, int mid, int high) {
+  int i = low;
+  int j = mid + 1;
+  int k = low;
+  unsigned long inversions = 0;
+  while ((i <= mid) && (j <= high)) {
+    int b = a[i];
+    int c = a[j];
+    //If using b<=c equal elements will be not readed as invertion
+    //For homework at stanford course this condition is required
+    if (b < c) {
       d[k] = b;
       i++;
     } else {
       d[k] = c;
-      invCount += hsize - i;
+      inversions += (mid - i + 1);
       j++;
+    }
+    k++;
+  }
+
+  if (i > mid) {
+    for (i = j; i <= high; i++) {
+      d[k] = a[i];
+      k++;
+    }
+  } else {
+    for (i; i <= mid; i++) {
+      d[k] = a[i];
+      k++;
     }
   }
 
-  for (int i = 0; i < n; i++) {
-    a[left + i] = d[i];
-  }
-
-  return invCount;
+  for (i = low; i <= high; i++) a[i] = d[i];
+  return inversions;
 }
 
-int mergesort(int a[], int left, int right) {
-  int result = 0;
+unsigned long mergesort(int a[], int temp[], int left, int right) {
+  unsigned long result = 0;
   if (left < right) {
     int mid = (left + right) / 2;
-    result += mergesort(a, left, mid);
-    result += mergesort(a, mid + 1, right);
-    result += merge(a, left, mid, right);
+    result += mergesort(a, temp, left, mid);
+    result += mergesort(a, temp, mid + 1, right);
+    result += merge(a, temp, left, mid, right);
   }
   return result;
 }
 
-int inversions(int a[], int len) {
-  if (len = 1)
+unsigned long inversions(int a[], int len) {
+  if (len == 1)
     return 0;
-  return mergesort(a, 0, len - 1);
+  int temp[len];
+  memcpy(temp, a, sizeof (a));
+  return mergesort(a, temp, 0, len - 1);
 }
+
+//https://d19vezwu8eufl6.cloudfront.net/algo1/slides%2Falgo-inversions2.pdf
+//https://gist.github.com/zulman/4720979
 
 int main(int argc, const char **argv) {
   redirect(argc, argv);
   vector<long> vec;
   int i = 0;
-  cin.ignore(1024, '\n');
   while (!cin.eof()) {
     cin >> i;
     vec.push_back(i);
   }
 
   int a [vec.size()];
-  //  long temp [vec.size()];
   copy(vec.begin(), vec.end(), a);
-  //  copy(vec.begin(), vec.end(), temp);
-  int r = inversions(a, vec.size());
-  printf("inversions: %d", r);
+  //  O(n^2)
+  //  unsigned long r = 0;
+  //  int n = vec.size();
+  //  for (int i = 0; i < n - 1; i++) {
+  //    for (int j = i + 1; j < n; j++) {
+  //      if (a[i] > a[j]) {
+  //        r++;
+  //      }
+  //    }
+  //  }
+  //  O(n*log(n))
+  unsigned long r = inversions(a, vec.size());
+  printf("items: %d, inversions: %lu", vec.size(), r);
   return 0;
 }
 
